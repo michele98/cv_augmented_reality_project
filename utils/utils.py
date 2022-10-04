5bbe013b7c6ddb6c213ec4bb369694d33425afb1
michele98 <calvanese.michele@gmail.com>
2022-10-04 17:01:45 +0200

    feat!: added F2F method

--- utils/utils.py
+++ utils/utils.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 
 from utils.matchers import FeatureMatcher
 
@@ -36,6 +37,7 @@ def frame_generator(filename):
     while(ret):
         yield cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         ret, frame = cap.read()
+    cap.release()
 
 
 def get_frame(filename, frame_number=0):
@@ -70,12 +72,12 @@ def get_frame(filename, frame_number=0):
     raise VideoFrameIndexError
 
 
-def overlay_ar(frame, reference_frame, ar_layer, ar_mask):
-    """Overlay the AR layer onto the video frame. The overlay is done
+def overlay_ar(frame, reference_frame, ar_layer, ar_mask, return_homography=False):
+    """Overlay the AR layer onto the video frame. The overlay is done}
     originally on the reference frame, then a homography between the
     reference frame and the original frame is computed using local
     invariant features matching.
-    
+
     The local invariant features are found using SIFT, the matches are
     found using FLANN KDTree, and finally the homography is computed
     on the resulting matches using RANSAC.
@@ -93,11 +95,18 @@ def overlay_ar(frame, reference_frame, ar_layer, ar_mask):
     ar_mask : image
         mask for the AR layer.
         Its resolution must be the same as ``original_frame``.
+    return_homography : bool
+        if True, the homography of the warping between the frame and
+        the reference frame is returned. By default False.
 
     Returns
     -------
     image
         the image with the overlaid AR layer.
+    image, 3x3 array
+        the image with the overlaid AR layer
+        the homography between the original frame and the reference
+        frame. Only if ``return_homography`` is set to True.
     """
     h, w = frame.shape[0], frame.shape[1]
 
@@ -111,11 +120,14 @@ def overlay_ar(frame, reference_frame, ar_layer, ar_mask):
     ar_frame = frame.copy()
     ar_frame[ar_mask_warped==255] = ar_layer_warped[ar_mask_warped==255]
 
+    if return_homography:
+        return ar_frame, H
     return ar_frame
 
 
 def save_ar_video(filename_src, filename_dst, ar_layer, ar_mask, reference_frame=None):
-    """Save the overlaid video
+    """Save the AR overlaid video with the F2R (frame to reference)
+    method.
 
     Parameters
     ----------
@@ -142,5 +154,65 @@ def save_ar_video(filename_src, filename_dst, ar_layer, ar_mask, reference_frame
         ar_frame = overlay_ar(frame, reference_frame, ar_layer, ar_mask)
         out.write(cv2.cvtColor(ar_frame, cv2.COLOR_RGB2BGR))
     out.release()
-    print(f"writing frame {i}", end = '\r')
-    print('done')
+    print(f"writing frame {i}")
+    print('done.')
+
+
+def save_ar_video_f2f(filename_src, filename_dst, ar_layer, ar_mask, reference_frame=None, drift_correction_step=0):
+    """Save the AR overlaid video with the F2F (frame to frame) method.
+
+    Parameters
+    ----------
+    filename : string
+        name of the source video file.
+    filename_dst : string
+        name of the destination video file.
+    ar_layer : image
+        image that needs to be overlaid onto the video.
+        Its resolution must be the same as ``original_frame``.
+    ar_mask : image
+        mask for the AR layer.
+        Its resolution must be the same as ``original_frame``.
+    reference_frame : image
+        reference frame onto which the AR layer is initially projected.
+        Its resolution must be the same as ``original_frame``.
+    drift_correction_step : int, optional
+        this much passes until the homography is recomputed using the
+        original reference frame. This is done to prevent drifting of
+        the AR layer. Setting this to 1 makes this effectively a F2R
+        matching method.
+    """
+    if reference_frame is None:
+        reference_frame = next(frame_generator(filename_src))
+    if drift_correction_step < 0:
+        raise ValueError("the drift correction step must be positive.")
+
+    out = cv2.VideoWriter(filename_dst, cv2.VideoWriter_fourcc(*'XVID'), 30, (640, 480))
+
+    H_original = np.eye(3)
+    ar_layer_original = ar_layer
+    ar_mask_original = ar_mask
+    reference_frame_original = reference_frame
+
+    H = H_original
+    for i, frame in enumerate(frame_generator(filename_src)):
+        print(f"writing frame {i}", end = '\r')
+
+        # reset the referernce frame to avoid excessive drift of the AR layer
+        if drift_correction_step>0 and i%drift_correction_step==0:
+            H = H_original
+            ar_layer = ar_layer_original
+            ar_mask = ar_mask_original
+            reference_frame = reference_frame_original
+
+        ar_frame, H_ar = overlay_ar(frame, reference_frame, ar_layer, ar_mask, return_homography=True)
+        reference_frame = frame
+
+        H = H@H_ar
+        h, w = ar_frame.shape[0], ar_frame.shape[1]
+        ar_layer = cv2.warpPerspective(ar_layer_original, H, dsize = (w, h))
+        ar_mask = cv2.warpPerspective(ar_mask_original, H, dsize = (w, h))
+        out.write(cv2.cvtColor(ar_frame, cv2.COLOR_RGB2BGR))
+    out.release()
+    print(f"writing frame {i}")
+    print('done.')
